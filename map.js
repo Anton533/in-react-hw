e4eff5ddce9d56e3914a96bc68ea149d4b1e714f
Anton <anton.omelianenko533@gmail.com>
2025-10-28 23:19:40 +0200

    in-react-hw-03

---
+++ map.js
@@ -0,0 +1,16 @@
+const employees = [
+  { name: "Alice", salary: 50000, experience: 3 },
+  { name: "Bob", salary: 60000, experience: 5 },
+  { name: "Charlie", salary: 45000, experience: 2 },
+];
+
+const newArray = employees.map((ele) => {
+  return {
+    name: ele.name,
+    salary: ele.salary,
+    experience: ele.experience,
+    totalSalary: ((ele.salary * 5) / 100) * ele.experience + ele.salary,
+  };
+});
+
+console.log(newArray);
